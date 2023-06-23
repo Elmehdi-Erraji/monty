d03ee168adbde9ec313a419c511da69f7757156b
Nizar-Bahajoub <116908630+Nizar-Bahajoub@users.noreply.github.com>
2023-06-23 02:03:31 +0100

    advanced3

---
+++ advanced3.c
@@ -0,0 +1,91 @@
+#include "monty.h"
+
+/**
+ * rotl - rotates the stack to the top
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void rotl(stack_t **stack, unsigned int nline)
+{
+	stack_t *temp;
+	int hold_this, hold_this_again;
+	(void)nline;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		nop(stack, nline);
+	}
+
+	hold_this = (*stack)->n;
+	temp = *stack;
+
+	while (temp)
+	{
+		if (temp->next == NULL)
+			break;
+		temp = temp->next;
+	}
+
+	hold_this_again = temp->n;
+	(*stack)->n = hold_this_again;
+	temp->n = hold_this;
+}
+
+/**
+ * rotlop - rotates stack to left
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void rotlop(stack_t **stack, unsigned int nline)
+{
+	stack_t *last, *tmp;
+
+	(void)nline;
+	if (!stack || !(*stack) || !((*stack)->next))
+		return;
+
+	tmp = *stack;
+	last = tmp;
+
+	while (last->next)
+	{
+		last = last->next;
+	}
+
+	last->next = tmp;
+	tmp->prev = last;
+	tmp->next->prev = NULL;
+	*stack = tmp->next;
+	tmp->next = NULL;
+}
+
+/**
+ * rotrop - rotates stack to right
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void rotrop(stack_t **stack, unsigned int nline)
+{
+	stack_t *last, *tmp;
+
+	(void)nline;
+	if (!stack || !(*stack) || !((*stack)->next))
+		return;
+
+	tmp = *stack;
+	last = tmp;
+
+	while (last->next)
+	{
+		last = last->next;
+	}
+
+	last->prev->next = NULL;
+	last->prev = NULL;
+	tmp->prev = last;
+	last->next = tmp;
+	*stack = last;
+}
