d6398c89ff54ad9574a498790a3755d6fcd3f844
Mehdi <elmahdi20erraji@gmail.com>
2023-06-23 01:51:23 +0100

    task done

---
+++ mandatory3.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - does literally nothing
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void nop(stack_t **stack, unsigned int nline)
+{
+	(void)stack;
+	(void)nline;
+}
