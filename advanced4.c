c31c4f71c8d0ed7be329e381814df8cddbccef1f
Mehdi <elmahdi20erraji@gmail.com>
2023-06-23 02:04:26 +0100

    task done

---
+++ advanced4.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+ * qpush - pushes for queue instead of stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void qpush(stack_t **stack, unsigned int nline)
+{
+	stack_t *last, *new;
+
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%d: stack not found\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	last = NULL;
+	if (*stack)
+	{
+		last = *stack;
+		while (last->next)
+			last = last->next;
+		last->next = new;
+	}
+	else
+		*stack = new;
+	new->prev = last;
+	new->next = NULL;
+	new->n = arg.arg;
+}
