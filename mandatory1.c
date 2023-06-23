9abc45af5cf5fc0b1e029138887bea848b35390a
Mehdi <elmahdi20erraji@gmail.com>
2023-06-23 01:49:15 +0100

    task done

---
+++ mandatory1.c
@@ -0,0 +1,81 @@
+#include "monty.h"
+
+/**
+ * push - pushes a node to the top of stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ *
+ * Return: Nothing.
+ */
+void push(stack_t **stack, unsigned int nline)
+{
+	stack_t *new;
+
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%d: stack not found\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	new->next = *stack;
+	new->prev = NULL;
+	new->n = arg.arg;
+	if (*stack)
+		(*stack)->prev = new;
+	*stack = new;
+}
+
+/**
+ * pall - prints the data of all nodes in stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ *
+ * Return: Nothing.
+ */
+void pall(stack_t **stack, unsigned int nline)
+{
+	stack_t *temp;
+	(void)nline;
+
+	temp = *stack;
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
+
+/**
+ * pint - prints the value at the top of stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void pint(stack_t **stack, unsigned int nline)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	while (temp)
+	{
+		if (temp->prev == NULL)
+			break;
+		temp = temp->prev;
+	}
+
+	printf("%d\n", temp->n);
+}
